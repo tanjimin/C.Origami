eefdb4d4120016e36ad2de3ac4a7af6f44248b28
tanjimin <tanjimin@hotmail.com>
2022-11-10 16:35:58 -0500

    file path changed

--- src/corigami/examples/prediction.sh
+++ src/corigami/examples/prediction.sh
@@ -4,7 +4,7 @@ python corigami/inference/prediction.py \
     --chr "chr2" \
     --celltype "imr90" \
     --start 500000 \
-    --model "../data/model/epoch=159-step=95199.ckpt" \
-    --seq "../data/data/hg38/dna_sequence" \
-    --ctcf "../data/data/hg38/imr90/genomic_features/ctcf_log2fc.bw" \
-    --atac "../data/data/hg38/imr90/genomic_features/atac.bw"
+    --model "../corigami_data/model_weights/corigami_base.ckpt" \
+    --seq "../corigami_data/data/hg38/dna_sequence" \
+    --ctcf "../corigami_data/data/hg38/imr90/genomic_features/ctcf_log2fc.bw" \
+    --atac "../corigami_data/data/hg38/imr90/genomic_features/atac.bw"
--- src/corigami/examples/screen.sh
+++ src/corigami/examples/screen.sh
@@ -3,10 +3,10 @@
 python corigami/inference/screening.py \
     --chr "chr2" \
     --celltype "imr90" \
-    --model "../data/model/epoch=159-step=95199.ckpt" \
-    --seq "../data/data/hg38/dna_sequence" \
-    --ctcf "../data/data/hg38/imr90/genomic_features/ctcf_log2fc.bw" \
-    --atac "../data/data/hg38/imr90/genomic_features/atac.bw" \
+    --model "../corigami_data/model_weights/corigami_base.ckpt" \
+    --seq "../corigami_data/data/hg38/dna_sequence" \
+    --ctcf "../corigami_data/data/hg38/imr90/genomic_features/ctcf_log2fc.bw" \
+    --atac "../corigami_data/data/hg38/imr90/genomic_features/atac.bw" \
     --screen-start 1250000 \
     --screen-end 2250000 \
     --perturb-width 1000 \
