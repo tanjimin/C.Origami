7862fde9c9ef82f4c2b45e7a43877fd1751731e5
tanjimin <tanjimin@hotmail.com>
2022-11-28 12:00:48 -0500

    training added

---
+++ src/corigami/training/main.py
@@ -0,0 +1,238 @@
+import sys
+import torch
+import argparse
+import numpy as np
+import pytorch_lightning as pl
+import pytorch_lightning.callbacks as callbacks
+
+import corigami.model.corigami_models as corigami_models
+from corigami.data import genome_dataset
+
+def main():
+    args = init_parser()
+    init_training(args)
+
+def init_parser():
+  parser = argparse.ArgumentParser(description='C.Origami Training Module.')
+
+  # Data and Run Directories
+  parser.add_argument('--seed', dest='run_seed', default=2077,
+                        help='Random seed for training')
+  parser.add_argument('--save_path', dest='run_save_path', default='checkpoints',
+                        help='Path to the model checkpoint')
+
+  # Data directories
+  parser.add_argument('--data-root', dest='dataset_data_root', default='data',
+                        help='Root path of training data', required=True)
+  parser.add_argument('--assembly', dest='dataset_assembly', default='hg38',
+                        help='Genome assembly for training data')
+  parser.add_argument('--celltype', dest='dataset_celltype', default='imr90',
+                        help='Sample cell type for prediction, used for output separation')
+
+  # Model parameters
+  parser.add_argument('--model-type', dest='model_type', default='ConvTransModel',
+                        help='CNN with Transformer')
+
+  # Training Parameters
+  parser.add_argument('--patience', dest='trainer_patience', default=80,
+                        help='Epoches before early stopping')
+  parser.add_argument('--max-epochs', dest='trainer_max_epochs', default=80,
+                        help='Max epochs')
+  parser.add_argument('--save-top-n', dest='trainer_save_top_n', default=20,
+                        help='Top n models to save')
+  parser.add_argument('--num_gpu', dest='trainer_num_gpu', default=4,
+                        help='Number of GPUs to use')
+
+  # Dataloader Parameters
+  parser.add_argument('--batch-size', dest='dataloader_batch_size', default=8,
+                        help='Batch size')
+  parser.add_argument('--ddp-disabled', dest='dataloader_ddp_disabled',
+                        action='store_false',
+                        help='Using ddp, adjust batch size')
+  parser.add_argument('--num-workers', dest='dataloader_num_workers', default=20,
+                        help='Dataloader workers')
+
+
+  args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+  return args
+
+def init_training(args):
+
+    # Early_stopping
+    early_stop_callback = callbacks.EarlyStopping(monitor='val_loss', 
+                                        min_delta=0.00, 
+                                        patience=args.trainer_patience,
+                                        verbose=False,
+                                        mode="min")
+    # Checkpoints
+    checkpoint_callback = callbacks.ModelCheckpoint(dirpath=f'{args.run_save_path}/models',
+                                        save_top_k=args.trainer_save_top_n, 
+                                        monitor='val_loss')
+
+    # LR monitor
+    lr_monitor = callbacks.LearningRateMonitor(logging_interval='epoch')
+
+    # Logger
+    csv_logger = pl.loggers.CSVLogger(save_dir = f'{args.run_save_path}/csv')
+    all_loggers = csv_logger
+    
+    # Assign seed
+    pl.seed_everything(args.run_seed, workers=True)
+    pl_module = TrainModule(args)
+    '''
+    pl_trainer = pl.Trainer(accelerator="gpu", devices=1,
+                            logger = [csv_logger, wandb_logger],
+                            callbacks = [early_stop_callback,
+                                         checkpoint_callback,
+                                         lr_monitor],
+                            max_epochs = args.trainer.max_epochs
+                            )
+    '''
+    pl_trainer = pl.Trainer(strategy='ddp',
+                            accelerator="gpu", devices=args.trainer_num_gpu,
+                            gradient_clip_val=1,
+                            logger = all_loggers,
+                            callbacks = [early_stop_callback,
+                                         checkpoint_callback,
+                                         lr_monitor],
+                            max_epochs = args.trainer_max_epochs
+                            )
+    trainloader = pl_module.get_dataloader(args, 'train')
+    valloader = pl_module.get_dataloader(args, 'val')
+    testloader = pl_module.get_dataloader(args, 'test')
+    pl_trainer.fit(pl_module, trainloader, valloader)
+
+class TrainModule(pl.LightningModule):
+    
+    def __init__(self, args):
+        super().__init__()
+        self.model = self.get_model(args)
+        self.args = args
+        self.save_hyperparameters()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def proc_batch(self, batch):
+        seq, features, mat, start, end, chr_name, chr_idx = batch
+        features = torch.cat([feat.unsqueeze(2) for feat in features], dim = 2)
+        inputs = torch.cat([seq, features], dim = 2)
+        mat = mat.float()
+        return inputs, mat
+    
+    def training_step(self, batch, batch_idx):
+        inputs, mat = self.proc_batch(batch)
+        outputs = self(inputs)
+        criterion = torch.nn.MSELoss()
+        loss = criterion(outputs, mat)
+
+        metrics = {'train_step_loss': loss}
+        self.log_dict(metrics, batch_size = inputs.shape[0], prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        ret_metrics = self._shared_eval_step(batch, batch_idx)
+        return ret_metrics
+
+    def test_step(self, batch, batch_idx):
+        ret_metrics = self._shared_eval_step(batch, batch_idx)
+        return ret_metrics
+
+    def _shared_eval_step(self, batch, batch_idx):
+        inputs, mat = self.proc_batch(batch)
+        outputs = self(inputs)
+        criterion = torch.nn.MSELoss()
+        loss = criterion(outputs, mat)
+        return loss
+
+    # Collect epoch statistics
+    def training_epoch_end(self, step_outputs):
+        step_outputs = [out['loss'] for out in step_outputs]
+        ret_metrics = self._shared_epoch_end(step_outputs)
+        metrics = {'train_loss' : ret_metrics['loss']
+                  }
+        self.log_dict(metrics, prog_bar=True)
+
+    def validation_epoch_end(self, step_outputs):
+        ret_metrics = self._shared_epoch_end(step_outputs)
+        metrics = {'val_loss' : ret_metrics['loss']
+                  }
+        self.log_dict(metrics, prog_bar=True)
+
+    def _shared_epoch_end(self, step_outputs):
+        loss = torch.tensor(step_outputs).mean()
+        return {'loss' : loss}
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), 
+                                     lr = 2e-4,
+                                     weight_decay = 0)
+
+        import pl_bolts
+        scheduler = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(optimizer, warmup_epochs=10, max_epochs=self.args.trainer_max_epochs)
+        scheduler_config = {
+            'scheduler': scheduler,
+            'interval': 'epoch',
+            'frequency': 1,
+            'monitor': 'val_loss',
+            'strict': True,
+            'name': 'WarmupCosineAnnealing',
+        }
+        return {'optimizer' : optimizer, 'lr_scheduler' : scheduler_config}
+
+    def get_dataset(self, args, mode):
+
+        celltype_root = f'{args.dataset_data_root}/{args.dataset_assembly}/{args.dataset_celltype}'
+        genomic_features = {'ctcf_log2fc' : {'file_name' : 'ctcf_log2fc.bw',
+                                             'norm' : None },
+                            'atac' : {'file_name' : 'atac.bw',
+                                             'norm' : 'log' }}
+        dataset = genome_dataset.GenomeDataset(celltype_root, 
+                                args.dataset_assembly,
+                                genomic_features, 
+                                mode = mode,
+                                include_sequence = True,
+                                include_genomic_features = True)
+
+        # Record length for printing validation image
+        if mode == 'val':
+            self.val_length = len(dataset) / args.dataloader_batch_size
+            print('Validation loader length:', self.val_length)
+
+        return dataset
+
+    def get_dataloader(self, args, mode):
+        dataset = self.get_dataset(args, mode)
+
+        if mode == 'train':
+            shuffle = True
+        else: # validation and test settings
+            shuffle = False
+        
+        if not args.dataloader_ddp_disabled:
+            gpus = args.trainer_num_gpu
+            batch_size = int(args.dataloader_batch_size / gpus)
+            num_workers = int(args.dataloader_num_workers / gpus) 
+        else:
+            batch_size = args.dataloader_batch_size
+            num_workers = args.dataloader_num_workers 
+        dataloader = torch.utils.data.DataLoader(
+            dataset,
+            shuffle = shuffle,
+            batch_size= batch_size,
+            num_workers=num_workers,
+            pin_memory=True,
+            prefetch_factor=1,
+            persistent_workers=True
+        )
+        return dataloader
+
+    def get_model(self, args):
+        model_name =  args.model_type
+        num_genomic_features = 2
+        ModelClass = getattr(corigami_models, model_name)
+        model = ModelClass(num_genomic_features, mid_hidden = 256)
+        return model
+
+if __name__ == '__main__':
+    main()
